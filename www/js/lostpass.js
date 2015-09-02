ac3d8b7dfff5b4c4e7c9ce7385967141897b741e
git <tolga@ozses.net>
2015-09-02 12:43:53 +0300

    Password reset implemented

--- www/js/lostpass.js
+++ www/js/lostpass.js
@@ -3,12 +3,16 @@ $("#lostpassbtn").on("click", function(){
     alert("Lütfen geçerli bir e-posta adresi giriniz")
   } else {
     $.ajax({
-      url: "http://www.rejicast.com/services/user/password_reset.json",
+      url: "http://www.rejicast.com/services/user/request_new_password.json",
       type: "post",
-      data: {
-        mail: encodeURIComponent($("#lostpass").val())
-      },
       dataType: "json",
+      data: {
+        name: encodeURIComponent($("#lostpass").val())
+      },
+      success: function() {
+        alert("Şifre sıfırlama talimatları kayıtlı e-posta adresinize gönderildi, lütfen kontrol ediniz.");
+      $.mobile.changePage("login.html", "slide");
+      },
     })
   }
 })
