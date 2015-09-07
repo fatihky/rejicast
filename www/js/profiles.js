8dca4a288b0657cf7dfb883a081937ab8ae6c993
git <tolga@ozses.net>
2015-09-08 02:20:54 +0300

    Restructured js files

--- www/js/profiles.js
+++ www/js/profiles.js
@@ -1,9 +1,4 @@
-$(function () {
-    $("#btn").on("click", getList);
-    $(".profile").on("click", getProfile);
-});
-
-function getList() {
+$("#btn").on("click", function() {
     $(".loader-container").show();
     $.ajax({
         url: 'http://www.rejicast.com/oyuncular.json',
@@ -11,13 +6,13 @@ function getList() {
         dataType: 'json',
         data: {
             "field_yasadigi_sehir_value": $("#city").val(),
-                "field_cinsiyet_value": $("#gender").val(),
-                "field_boy_value": $("#height").val(),
-                "field_goz_rengi_value": $("#eyecolour").val(),
-                "field_ten_rengi_value": $("#skincolour").val(),
-                "field_kilo_value": $("#weight").val(),
-                "field_egitim_duzeyi_value": $("#education").val(),
-                "field_kategorisi_value": $("#category").val()
+            "field_cinsiyet_value": $("#gender").val(),
+            "field_boy_value": $("#height").val(),
+            "field_goz_rengi_value": $("#eyecolour").val(),
+            "field_ten_rengi_value": $("#skincolour").val(),
+            "field_kilo_value": $("#weight").val(),
+            "field_egitim_duzeyi_value": $("#education").val(),
+            "field_kategorisi_value": $("#category").val()
         },
         success: function (data) {
             $("label").remove();
@@ -33,24 +28,21 @@ function getList() {
                 var profile = $('<div class="profile"><img class="profileImage" data-nid="' + value.node.nid + '" src="' + value.node.field_oyuncu_fotografi.src + '"><div class="profileName">' + value.node.field_gosterilecek_ad + '</div></div>');
                 $("#contentHolder.profiles").append(profile);
             });
+            $(".profileImage").on("click", function(ev) {
+                $(".loader-container").show();
+                var nid=$(ev.target).attr("data-nid");
+                $.ajax({
+                    url: 'http://www.rejicast.com/oyuncu.json',
+                    type: 'get',
+                    dataType: 'json',
+                    data: {
+                        nid: nid,
+                    },
+                    success: function(data) {
+                        console.log(JSON.stringify(data));
+                    },
+                })
+            });
         }
     });
-}
-
-function getProfile(e) {
-    /*
-    $(".loader-container").show();
-    $.ajax({
-        url: 'http://www.rejicast.com/oyuncu.json',
-        type: 'get',
-        dataType: 'json',
-        data: {
-            uid: e.nid,
-        },
-        success: function(data) {
-            $(".profile").hide();
-        },
-    })
-    */
-    alert('Clicked!');
-}
+});
