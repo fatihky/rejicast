0b58eb1297abf930a734e9d3a3ba9b22ae345d42
git <tolga@ozses.net>
2015-09-10 23:34:03 +0300

    Added event handlers and changed script loading order

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,30 @@
-$("#navHolder #homeBtn").on("click", function(){
-  $.mobile.changePage("index.html", "slide");
-});
-$("#navHolder #notificationBtn").on("click", function(){
-  $.mobile.changePage("notifications.html", "slide");
+document.addEventListener("deviceready", startUp, false);
+document.addEventListener("resume", startUp, false);
+
+function startUp() {
+  $("#navHolder #homeBtn").on("click", function(){
+    $.mobile.changePage("index.html", "slide");
+  });
+  $("#navHolder #notificationBtn").on("click", function(){
+    $.mobile.changePage("notifications.html", "slide");
+    $.ajax({
+      url: 'http://www.rejicast.com/genel-duyurular.json',
+      type: 'get',
+      dataType: 'json',
+      success: function (data) {
+        $.each(data.nodes, function (key,value) {
+          var announcement = $('<div class="generalNotification  ' + value.node.field_ozel_duyuru +' '+ value.node.field_kime +'"><h3>' + value.node.title + '</h3><p>' + value.node.field_icerik + '</div>');
+          $('#contentHolder.notifications').append(announcement);
+        });
+      }
+    })
+  });
   $.ajax({
-    url: 'http://www.rejicast.com/genel-duyurular.json',
-    type: 'get',
+    url: 'http://www.rejicast.com/services/system/connect',
+    type: 'post',
     dataType: 'json',
-    success: function (data) {
-      $.each(data.nodes, function (key,value) {
-        var announcement = $('<div class="generalNotification  ' + value.node.field_ozel_duyuru +' '+ value.node.field_kime +'"><h3>' + value.node.title + '</h3><p>' + value.node.field_icerik + '</div>');
-        $('#contentHolder.notifications').append(announcement);
-      });
+    success: function(data) {
+      console.log(JSON.stringify(data));
     }
-  })
-});
+  });
+}
