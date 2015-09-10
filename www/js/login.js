36f6f053f75e05e763d6c2669a5fae0a77a2b1c6
git <tolga@ozses.net>
2015-09-10 15:41:08 +0300

    Fixed the CSRF not validating

--- www/js/login.js
+++ www/js/login.js
@@ -1,4 +1,6 @@
 $('#login').on('click', function () {
+    $(".loader-container > p").text("Giriş yapılıyor");
+    $(".loader-container").show();
     var name = $('#username').val();
     if (!name) {
         alert("Lütfen kullanıcı adınızı girin.");
@@ -10,58 +12,67 @@ $('#login').on('click', function () {
         return false;
     }
     $.ajax({
-        url: 'http://www.rejicast.com/services/user/login.json',
+        url: 'http://www.rejicast.com/services/user/token.json',
         type: 'post',
         dataType: 'json',
-        data: 'username='+name.trim()+'&password='+pass,
-        success: function (data) {
+        success: function(token) {
             $.ajax({
-                url: 'http://www.rejicast.com/oyuncularim.json',
-                type: 'get',
+                url: 'http://www.rejicast.com/services/user/login.json',
+                type: 'post',
                 dataType: 'json',
-                data: {
-                    uid: data.user.uid
-                },
-                statusCode: {
-                    401: function () {
-                        alert('Kullanıcı adı ya da şifre hatalı.');
-                    }
+                data: 'username='+name.trim()+'&password='+pass,
+                beforeSend: function(request) {
+                    request.setRequestHeader("X-CSRF-Token", token.token);
                 },
                 success: function (data) {
-                    $(".loader-container").text("Giriş yapılıyor");
-                    $(".loader-container").show();
-                    $("#contentHolder.login > #username").remove();
-                    $("#contentHolder.login > #password").remove();
-                    $("#contentHolder.login > #submit").remove();
-                    setTimeout(function () {
+                    $(".loader-container > p").text("Hoşgeldin "+data.user.name);
+                    setTimeout(function() {
                         $(".loader-container").fadeOut(500);
                     }, 1000);
-                    $.each(data.nodes, function (key, value) {
-                        var profile = $('<div class="profile"><img class="profileImage" data-nid="' + value.node.nid + '" src="' + value.node.field_oyuncu_fotografi.src + '"><div class="profileName">' + value.node.title+ '</div></div>');
-                        $("#contentHolder.login").append(profile);
-                    });
-                    $(".profileImage").on("click", function(ev) {
-                        $(".loader-container > p").text('Oyuncu bilgileri alınıyor');
-                        $(".loader-container").show();
-                        var nid=$(ev.target).attr("data-nid");
-                        $.ajax({
-                            url: 'http://www.rejicast.com/oyuncu.json',
-                            type: 'get',
-                            dataType: 'json',
-                            data: {
-                                nid: nid,
-                            },
-                            success: function(data) {
-                                $(".loader-container").remove();
-                                $("#contentHolder.login > .profile").remove();
-                                var profile = $('<img class="singleProfileImage" src="' + data.nodes[0].node.field_oyuncu_fotografi.src + '"><div class="singleProfileName">' + data.nodes[0].node.title+ '</div><div class="singleProfileSkin"><div class="singleProfileLabel">Ten Rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ten_rengi + '</div></div>');
+                    $.ajax({
+                        url: 'http://www.rejicast.com/oyuncularim.json',
+                        type: 'get',
+                        dataType: 'json',
+                        data: {
+                            uid: data.user.uid
+                        },
+                        statusCode: {
+                            401: function () {
+                                alert('Kullanıcı adı ya da şifre hatalı.');
+                            }
+                        },
+                        success: function (data) {
+                            $("#contentHolder.login > .ui-input-text").remove();
+                            $("#contentHolder.login > .ui-link").remove();
+                            $("#contentHolder.login > #login").remove();
+                            $.each(data.nodes, function (key, value) {
+                                var profile = $('<div class="profile"><img class="profileImage" data-nid="' + value.node.nid + '" src="' + value.node.field_oyuncu_fotografi.src + '"><div class="profileName">' + value.node.field_gosterilecek_ad+ '</div></div>');
                                 $("#contentHolder.login").append(profile);
-                            },
-                        });
+                            });
+                            $(".profileImage").on("click", function(ev) {
+                                $(".loader-container > p").text('Oyuncu bilgileri alınıyor');
+                                $(".loader-container").show();
+                                var nid=$(ev.target).attr("data-nid");
+                                $.ajax({
+                                    url: 'http://www.rejicast.com/oyuncu.json',
+                                    type: 'get',
+                                    dataType: 'json',
+                                    data: {
+                                        nid: nid,
+                                    },
+                                    success: function(data) {
+                                        $(".loader-container").hide();
+                                        $("#contentHolder.login > .profile").remove();
+                                        var profile = $('<img class="singleProfileImage" src="' + data.nodes[0].node.field_oyuncu_fotografi.src + '"><div class="singleProfileName">' + data.nodes[0].node.field_gosterilecek_ad+ '</div><div class="singleProfileSkin"><div class="singleProfileLabel">Ten Rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ten_rengi + '</div></div>');
+                                        $("#contentHolder.login").append(profile);
+                                    },
+                                });
+                            });
+                        },
                     });
-                },
+                }
             });
-        }
+            return false;
+        },
     });
-    return false;
 });
