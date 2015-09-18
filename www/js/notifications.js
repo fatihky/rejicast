225580ed834abd95cf9ca752e41d630c40e22276
git <tolga@ozses.net>
2015-09-18 16:52:36 +0300

    Notification and its styling fixed

--- www/js/notifications.js
+++ www/js/notifications.js
@@ -1,15 +1,39 @@
-  $.ajax({
-    url: 'http://www.rejicast.com/duyurular.json',
-    type: 'get',
+$.ajax({
+    url: 'http://www.rejicast.com/services/user/token.json',
+    type: 'post',
     dataType: 'json',
-    success: function (data) {
-      console.log(JSON.stringify(data));
-      $.each(data.nodes, function (key,value) {
-        var announcement = $('<div class="generalNotification  ' + value.node.field_ozel_duyuru +' '+ value.node.field_kime +'"><h3>' + value.node.title + '</h3><p>' + value.node.field_icerik + '</div>');
-        $('#contentHolder.notifications').append(announcement);
-      });
-    },
-    error: function(xhr,status,message) {
-      console.log(message);
+    success: function(token) {
+        $.ajax({
+            url: 'http://www.rejicast.com/services/system/connect.json',
+            type: 'post',
+            dataType: 'json',
+            beforeSend: function(r) {
+                r.setRequestHeader("X-CSRF-Token", token.token)
+            },
+            success: function(connect) {
+                console.log(JSON.stringify(connect));
+                $.ajax({
+                    url: 'http://www.rejicast.com/duyurular.json',
+                    type: 'get',
+                    dataType: 'json',
+                    success: function (data) {
+                        $.each(data.nodes, function (key,value) {
+                            var announcement = $('<div class="generalNotification ' + value.node.field_ozel_duyuru +' '+ value.node.field_kime +'"><h3>' + value.node.title + '</h3><p>' + value.node.field_icerik + '</div>');
+                            $('#contentHolder.notifications').append(announcement);
+                        });
+                        $('.evet:not(.'+connect.user.uid+')').remove();
+                    }
+                });
+            }
+        });
     }
-  });
+});
+
+
+
+$('#selfNot').click(function(){
+    $('.hayir').hide();
+});
+$('#genNot').click(function(){
+    $('.hayir').show();
+});
