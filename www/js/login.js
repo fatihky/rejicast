3c5744f4647c56e9c8b5ba39f1a5adee41cbf9bd
git <tolga@ozses.net>
2015-09-28 16:54:13 +0300

    Changed alert parameters

--- www/js/login.js
+++ www/js/login.js
@@ -3,12 +3,12 @@ $('#login').on('click', function () {
     $(".loader-container").show();
     var name = $('#username').val();
     if (!name) {
-        navigator.notification.alert("Lütfen kullanıcı adınızı girin.");
+        navigator.notification.alert("Lütfen kullanıcı adınızı girin.", function(){return;}, "Hata", "Tamam");
         return false;
     }
     var pass = $('#password').val();
     if (!pass) {
-        navigator.notification.alert("Lütfen şifrenizi girin.");
+        navigator.notification.alert("Lütfen şifrenizi girin.", function(){return;}, "Hata", "Tamam");
         return false;
     }
     $.ajax({
@@ -26,7 +26,7 @@ $('#login').on('click', function () {
                 },
                 statusCode: {
                     401: function() {
-                        navigator.notification.alert("Kullanıcı adı ya da şifreniz yanlış");
+                        navigator.notification.alert("Kullanıcı adı ya da şifreniz yanlış", function(){return;}, "Hata", "Tamam");
                     }
                 },
                 success: function (data) {
@@ -40,13 +40,18 @@ $('#login').on('click', function () {
                     $("#logoutBtn").css("display","block");
                     $("#loginHolder").text(data.user.name);
                     window.location.href = "index.html";
-                    /*
-                    $.mobile.changePage("index.html", {reloadPage:true},{allowSamePageTransition:true},{transition:'slide'});
-                    */
                     checkStatus();
                 }
             });
             return false;
         },
     });
+});
+$("#shareViaEmail #email").on("click", function() {
+    console.log('Clicked the email icon');
+    window.plugins.socialsharing.shareViaEmail('http://www.rejicast.com/node/'+nid);
+});
+$("#shareViaWhatsApp #whatsapp").on("click", function() {
+    console.log('Clicked the whatsaap icon');
+    window.plugins.socialsharing.shareViaWhatsApp('http://www.rejicast.com/node/'+nid);
 });
