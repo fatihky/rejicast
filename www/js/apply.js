a76d3f44b976972c16e43139ca4fab1535747126
git <tolga@ozses.net>
2015-09-28 14:03:34 +0300

    Changed alert parameters, logging more verbosely now and added data type

--- www/js/apply.js
+++ www/js/apply.js
@@ -33,25 +33,20 @@ $(function() {
   $("#privacy").on("change", function() {
     $("#submit").toggleClass("hidden");
   });
-  $("a#inst").on('tap', function(event) {
-    window.open('inst.html');
-    event.stopPropagation();
-  });
   $("#submit").on("click", function() {
     $.ajax({
       url:'http://www.rejicast.com/services/create.json',
       type:'post',
-      data: $("basvuru_formu").serializeArray(),
-      //data:'node[type]=oyuncu&node[title]='+encodeURIComponent($("#name").val())+'&node[language]=und&node[field_tc_kimlik_no][und][0][value]='+encodeURIComponent($("#tckn").val())+'&node[field_sgk_durumu][und][0][value]='+encodeURIComponent($("#sgk option:selected").val())+'&node[field_telefon][und][0][value]='+encodeURIComponent($("#tel").val())+'&node[field_telefon_2][und][0][value]='+encodeURIComponent($("#tel2").val())+'&node[field_adres][und][0][value]='+encodeURIComponent($("#address").val())+'&node[field_yasadigi_sehir][und][0][value]='+encodeURIComponent($("#city option:selected").val())+'&node[field_kategorisi][und][0][value]='+encodeURIComponent($("input[name='kategori']:checked").val())+'&node[field_dogum_tarihi][und][0][value]='+encodeURIComponent($("#dob").val())+'&node[field_boy][und][0][value]='+encodeURIComponent($("select#height option:checked").val())+'&node[field_kilo][und][0][value]='+encodeURIComponent($("select#weight option:checked").val())+'&node[field_goz_rengi][und][0][value]='+encodeURIComponent($("select#eyecolour option:checked").val())+'&node[field_ten_rengi][und][0][value]='+encodeURIComponent($("select#skincolour option:checked").val())+'&node[field_ayak_no][und][0][value]='+encodeURIComponent($("select#shoesize option:checked").val())+'&node[field_egitim_duzeyi][und][0][value]='+encodeURIComponent($("select#education option:checked").val())+'&node[field_oyunculuk_egitimleri][und][0][value]='+encodeURIComponent($("#training").val())+'&node[field_diller][und][0][value]='+encodeURIComponent($("#languages").val())+'&node[field_beceriler][und][0][value]='+encodeURIComponent($("#skills").val())+'&node[field_kisisel][und][0][value]='+encodeURIComponent($("#personal").val())+'&node[field_videolar[und][0][video_url]]='+encodeURIComponent($(".videolink").val())+'&node[field_videolar][und][0][description]='+encodeURIComponent($(".videodesc").val())+'&node[field_okudum_anladim][und][0][value]='+encodeURIComponent($(".privacy").is(':checked')?1:0),
+      dataType:'json',
+      data:'node[type]=oyuncu&node[title]='+encodeURIComponent($("#name").val())+'&node[language]=und&node[field_tc_kimlik_no][und][value]='+encodeURIComponent($("#tckn").val())+'&node[field_sgk_durumu][und][value]='+encodeURIComponent($("#sgk option:selected").val())+'&node[field_telefon][und][value]='+encodeURIComponent($("#tel").val())+'&node[field_telefon_2][und][value]='+encodeURIComponent($("#tel2").val())+'&node[field_adres][und][value]='+encodeURIComponent($("#address").val())+'&node[field_yasadigi_sehir][und][value]='+encodeURIComponent($("#city option:selected").val())+'&node[field_kategorisi][und][value]='+encodeURIComponent($("input[name='kategori']:checked").val())+'&node[field_dogum_tarihi][und][value]='+encodeURIComponent($("#dob").val())+'&node[field_boy][und][value]='+encodeURIComponent($("select#height option:checked").val())+'&node[field_kilo][und][value]='+encodeURIComponent($("select#weight option:checked").val())+'&node[field_goz_rengi][und][value]='+encodeURIComponent($("select#eyecolour option:checked").val())+'&node[field_ten_rengi][und][value]='+encodeURIComponent($("select#skincolour option:checked").val())+'&node[field_ayak_no][und][value]='+encodeURIComponent($("select#shoesize option:checked").val())+'&node[field_egitim_duzeyi][und][value]='+encodeURIComponent($("select#education option:checked").val())+'&node[field_oyunculuk_egitimleri][und][value]='+encodeURIComponent($("#training").val())+'&node[field_diller][und][value]='+encodeURIComponent($("#languages").val())+'&node[field_beceriler][und][value]='+encodeURIComponent($("#skills").val())+'&node[field_kisisel][und][value]='+encodeURIComponent($("#personal").val())+'&node[field_videolar[und][video_url]='+encodeURIComponent($(".videolink").val())+'&node[field_videolar][und][description]='+encodeURIComponent($(".videodesc").val())+'&node[field_okudum_anladim][und][value]='+encodeURIComponent($(".privacy").is(':checked')?1:0),
       success:function() {
         window.location.href = "thanks.html";
-        /*
-        $mobile.changePage("thanks.html", "slideup");
-        */
       },
       error:function(xhr,status,message) {
+        console.log(xhr);
+        console.log(status);
         console.log(message);
-        navigator.notification.alert("Kaydınızı oluşturamadık, daha sonra tekrar deneyin.");
+        navigator.notification.alert("Kaydınızı oluşturamadık, daha sonra tekrar deneyin.", function(){return;}, "Hata", "Tamam");
       }
     });
   });
