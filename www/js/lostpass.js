ecbd11dc4772648a9d8a7c09a8a0100ce2720ad4
git <tolga@ozses.net>
2015-09-28 16:54:49 +0300

    Restructured js file

--- www/js/lostpass.js
+++ www/js/lostpass.js
@@ -1,23 +1,18 @@
-$(function() {
-  $("#lostpassbtn").on("click", function(){
-    if (!$("#lostpass").val()) {
-      navigator.notification.alert("Lütfen kullanıcı adınızı girin.");
-    } else {
-      $.ajax({
-        url: "http://www.rejicast.com/services/user/request_new_password.json",
-        type: "post",
-        dataType: "json",
-        data: {
-          name: encodeURIComponent($("#lostpass").val())
-        },
-        success: function() {
-          navigator.notification.alert("Şifre sıfırlama talimatları kayıtlı e-posta adresinize gönderildi, lütfen kontrol ediniz.");
-          window.location.href = "index.html";
-          /*
-          $.mobile.changePage("login.html", "slide");
-          */
-        },
-      })
-    }
-  });
+$("#lostpassbtn").on("click", function(){
+  if (!$("#lostpass").val()) {
+    navigator.notification.alert("Lütfen kullanıcı adınızı girin.");
+  } else {
+    $.ajax({
+      url: "http://www.rejicast.com/services/user/request_new_password.json",
+      type: "post",
+      dataType: "json",
+      data: {
+        name: encodeURIComponent($("#lostpass").val())
+      },
+      success: function() {
+        navigator.notification.alert("Şifre sıfırlama talimatları kayıtlı e-posta adresinize gönderildi, lütfen kontrol ediniz.", function(){return;}, "Bilgi", "Tamam");
+        window.location.href = "index.html";
+      },
+    })
+  }
 });
