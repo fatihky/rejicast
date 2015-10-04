d6e92c5ce5f568ff4dc81f60374eb44da4d39f93
git <tolga@ozses.net>
2015-10-04 19:39:29 +0300

    Working on submitting and image at the same time

--- www/js/apply.js
+++ www/js/apply.js
@@ -1,6 +1,5 @@
 document.addEventListener("deviceready", function() {
   $("#uploadimage").on("click", function() {
-    console.log("Clicked");
     navigator.camera.getPicture(onSuccess, onFail, {
       quality:100,
       allowEdit:true,
@@ -15,11 +14,12 @@ document.addEventListener("deviceready", function() {
 function onSuccess(imageData) {
   var image = $("img#image");
   image.attr("src", "data:image/jpeg;base64," + imageData);
+  image.css("display", "block");
   var data = {
     "file":{
       "file":imageData,
-      "filename":"deneme.jpg",
-      "filepath":"public://deneme.jpg"
+      "filename":"rejicast.jpg",
+      "filepath":"public://"+imageData.replace(/\//g,"").replace(/\+/g,"").substr(0,10)+".jpg"
     }
   };
 }
@@ -28,21 +28,6 @@ function onFail() {
     navigator.notification.alert("Bir hata oluştu", function(){return;}, "Hata", "Tamam");
   }, 0);
 }
-$(document).ready(function() {
-  $('#profilepicture #image').cropper();
-  $('#profilepicture #image').cropper({
-    aspectRatio: 16 / 9,
-    crop: function(e) {
-      console.log(e.x);
-      console.log(e.y);
-      console.log(e.width);
-      console.log(e.height);
-      console.log(e.rotate);
-      console.log(e.scaleX);
-      console.log(e.scaleY);
-    }
-  });
-});
 $("#addmoreprods").on("click", function () {
   var tr = $(".prods").first().clone();
   tr.find('input').val('');
@@ -58,21 +43,25 @@ $("#tel2").mask("999 999-9999");
 $("#privacy").on("change", function() {
   $("#submit").toggleClass("hidden");
 });
-$("#submit").on("click", function(imageData) {
+$("#submit").on("click", function(imageData,data) {
+  console.log(imageData);
+  console.log(JSON.stringify(data));
+      /*
   $.ajax({
-    url:'http://www.rejicast.com/services/create.json',
+    url:'http://www.rejicast.com/services/file.json',
     type:'post',
     dataType:'json',
-    data:'node[type]=oyuncu&node[title]='+encodeURIComponent($("#name").val())+'&node[language]=und&node[field_tc_kimlik_no][und][0][value]='+encodeURIComponent($("#tckn").val())+'&node[field_sgk_durumu][und][value]='+encodeURIComponent($("#sgk option:selected").val())+'&node[field_telefon][und][0][value]='+encodeURIComponent($("#tel").val())+'&node[field_telefon_2][und][0][value]='+encodeURIComponent($("#tel2").val())+'&node[field_adres][und][0][value]='+encodeURIComponent($("#address").val())+'&node[field_yasadigi_sehir][und][value]='+encodeURIComponent($("#city option:selected").val())+'&node[field_e_posta][und][0][value]='+encodeURIComponent($("#email").val())+'&node[field_cinsiyet][und][value]='+encodeURIComponent($("#gender option:selected").val())+'&node[field_kategorisi][und][0][value]='+encodeURIComponent($("input[name='kategori']:checked").val())+'&node[field_dogum_tarihi][und][value]='+encodeURIComponent($("#dob").val())+'&node[field_boy][und][value]='+encodeURIComponent($("select#height option:selected").val())+'&node[field_kilo][und][value]='+encodeURIComponent($("select#weight option:selected").val())+'&node[field_goz_rengi][und][value]='+encodeURIComponent($("select#eyecolour option:selected").val())+'&node[field_ten_rengi][und][value]='+encodeURIComponent($("select#skincolour option:selected").val())+'&node[field_ayak_no][und][value]='+encodeURIComponent($("select#shoesize option:selected").val())+'&node[field_egitim_duzeyi][und][value]='+encodeURIComponent($("select#education option:selected").val())+'&node[field_oyunculuk_egitimleri][und][0][value]='+encodeURIComponent($("#training").val())+'&node[field_diller][und][0][value]='+encodeURIComponent($("#languages").val())+'&node[field_beceriler][und][0][value]='+encodeURIComponent($("#skills").val())+'&node[field_kisisel][und][0][value]='+encodeURIComponent($("#personal").val())+'&node[field_videolar[und][0][video_url]='+encodeURIComponent($(".videolink").val())+'&node[field_videolar][und][0][description]='+encodeURIComponent($(".videodesc").val())+'&node[field_okudum_anladim][und][value]='+encodeURIComponent($(".privacy").is(':selected')?1:0),
-    /* File upload */
-    success:function() {
+    data:imageData.data,
+    success:function(res) {
+      console.log('result: '+JSON.stringify(res));
       $.ajax({
-        url:'http://www.rejicast.com/services/file.json',
+        url:'http://www.rejicast.com/services/create.json',
         type:'post',
         dataType:'json',
-        data:data,
-        success:function(res) {
-          console.log(JSON.stringify(res));
+        data:'node[type]=hede&node[language]=und&node[title]=Hede&node[field_gorsel][und][0][upload]'+res.fid,
+        //data:'node[type]=oyuncu&node[field_oyuncu_fotografi][und][0][upload]='+res.fid+'node[title]='+encodeURIComponent($("#name").val())+'&node[language]=und&node[field_tc_kimlik_no][und][0][value]='+encodeURIComponent($("#tckn").val())+'&node[field_sgk_durumu][und][value]='+encodeURIComponent($("#sgk option:selected").val())+'&node[field_telefon][und][0][value]='+encodeURIComponent($("#tel").val())+'&node[field_telefon_2][und][0][value]='+encodeURIComponent($("#tel2").val())+'&node[field_adres][und][0][value]='+encodeURIComponent($("#address").val())+'&node[field_yasadigi_sehir][und][value]='+encodeURIComponent($("#city option:selected").val())+'&node[field_e_posta][und][0][value]='+encodeURIComponent($("#email").val())+'&node[field_cinsiyet][und][value]='+encodeURIComponent($("#gender option:selected").val())+'&node[field_kategorisi][und][0][value]='+encodeURIComponent($("input[name='kategori']:checked").val())+'&node[field_dogum_tarihi][und][value]='+encodeURIComponent($("#dob").val())+'&node[field_boy][und][value]='+encodeURIComponent($("select#height option:selected").val())+'&node[field_kilo][und][value]='+encodeURIComponent($("select#weight option:selected").val())+'&node[field_goz_rengi][und][value]='+encodeURIComponent($("select#eyecolour option:selected").val())+'&node[field_ten_rengi][und][value]='+encodeURIComponent($("select#skincolour option:selected").val())+'&node[field_ayak_no][und][value]='+encodeURIComponent($("select#shoesize option:selected").val())+'&node[field_egitim_duzeyi][und][value]='+encodeURIComponent($("select#education option:selected").val())+'&node[field_oyunculuk_egitimleri][und][0][value]='+encodeURIComponent($("#training").val())+'&node[field_diller][und][0][value]='+encodeURIComponent($("#languages").val())+'&node[field_beceriler][und][0][value]='+encodeURIComponent($("#skills").val())+'&node[field_kisisel][und][0][value]='+encodeURIComponent($("#personal").val())+'&node[field_videolar[und][0][video_url]='+encodeURIComponent($(".videolink").val())+'&node[field_videolar][und][0][description]='+encodeURIComponent($(".videodesc").val())+'&node[field_okudum_anladim][und][value]='+encodeURIComponent($(".privacy").is(':selected')?1:0),
+        success:function(data) {
+          console.log(JSON.stringify(data));
           window.location.href = "thanks.html";
         },
         error:function(xhr,status,message) {
@@ -82,6 +71,27 @@ $("#submit").on("click", function(imageData) {
           navigator.notification.alert("Kaydınızı oluşturamadık, daha sonra tekrar deneyin.", function(){return;}, "Hata", "Tamam");
         }
       });
+    },
+   error:function(xhr,status,message) {
+     console.log(xhr);
+     console.log(status);
+     console.log(message);
+   }
+  });
+});
+$(document).ready(function() {
+  $('#profilepicture #image').cropper();
+  $('#profilepicture #image').cropper({
+    aspectRatio: 16 / 9,
+    crop: function(e) {
+      console.log(e.x);
+      console.log(e.y);
+      console.log(e.width);
+      console.log(e.height);
+      console.log(e.rotate);
+      console.log(e.scaleX);
+      console.log(e.scaleY);
     }
   });
+    */
 });
