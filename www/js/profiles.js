867877e34c42b930fe6e016b28b9354e8c500602
git <tolga@ozses.net>
2015-11-24 16:30:19 +0200

    Profile WIP

--- www/js/profiles.js
+++ www/js/profiles.js
@@ -1,4 +1,4 @@
-$("#btn").on("click", function() {
+$("#search").on("click", function() {
     $(".loader-container > p").text("Lütfen bekleyin, liste yükleniyor");
     $(".loader-container").show();
     $.ajax({
@@ -16,10 +16,6 @@ $("#btn").on("click", function() {
             "field_kategorisi_value": $("#category").val()
         },
         success: function (data) {
-            console.log(JSON.stringify(data));
-            $("#contentHolder.profiles label").remove();
-            $("#contentHolder.profiles select").remove();
-            $("#contentHolder.profiles #btn").remove();
             if (data.nodes.length === 0) {
                 $(".container").fadeOut(50);
                 $(".loader-container").hide();
@@ -28,8 +24,8 @@ $("#btn").on("click", function() {
             $(".loader-container").fadeOut(500);
             $.each(data.nodes, function (key, value) {
                 profile = $('<div class="profile"><img class="profileImage" data-nid="' + value.node.nid + '" src="' + value.node.field_oyuncu_fotografi.src + '"><div class="profileName">' + value.node.field_gosterilecek_ad + '</div></div>');
-                $("#contentHolder.profiles").append(profile);
-                window.scrollTo(0,0);
+                $("#profiles").append(profile);
+                window.location.href = "#profiles";
             });
             $(".profileImage").on("click", function(ev) {
                 $(".loader-container > p").text('Oyuncu bilgileri alınıyor');
@@ -45,29 +41,28 @@ $("#btn").on("click", function() {
                     success: function(data) {
                         var category = data.nodes[0].node.field_kategorisi.indexOf("Stand hostesi") != -1;
                         $(".loader-container").hide();
-                        $("#contentHolder.profiles > .profile").hide();
                         var share = $('<div id="shareViaEmail"><img id="email" src="img/email.png"/></div><div id="shareViaWhatsApp"><img id="whatsapp" src="img/whatsapp.png"/></div>');
                         var profile = $('<img class="singleProfileImage" src="' + data.nodes[0].node.field_oyuncu_fotografi.src + '"><div class="singleProfileName">' + data.nodes[0].node.field_gosterilecek_ad + '</div><div class="singleProfileLabel">Cinsiyet:</div><div class="singleProfileContent">' + data.nodes[0].node.field_cinsiyet + '</div><div class="singleProfileLabel">Doğum tarihi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_dogum_tarihi + '</div><div class="singleProfileLabel">Şehir:</div><div class="singleProfileContent">' + data.nodes[0].node.field_yasadigi_sehir + '</div><div class="singleProfileLabel">Kategorisi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kategorisi + '</div><div class="singleProfileLabel">Eğitim düzeyi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_egitim_duzeyi + '</div><div class="singleProfileLabel">Diller:</div><div class="singleProfileContent">' + data.nodes[0].node.field_diller + '</div><div class="singleProfileLabel">Kişisel:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kisisel + '</div><div class="singleProfileLabel">Boy:</div><div class="singleProfileContent">' + data.nodes[0].node.field_boy + '</div><div class="singleProfileLabel">Kilo:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kilo + '</div><div class="singleProfileLabel">Ten Rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ten_rengi + '</div>');
-                        $("#contentHolder.profiles").append(profile);
+                        $("#profile").append(profile);
+                        window.location.href = "#profile";
                         if (category) {
-                            $("#contentHolder.profiles").append('</div><div class="singleProfileLabel">Göğüs:</div><div class="singleProfileContent">' + data.nodes[0].node.field_gogus + '</div><div class="singleProfileLabel">Bel:</div><div class="singleProfileContent">' + data.nodes[0].node.field_bel + '</div><div class="singleProfileLabel">Kalça:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kalca);
+                            $("#profile").append('</div><div class="singleProfileLabel">Göğüs:</div><div class="singleProfileContent">' + data.nodes[0].node.field_gogus + '</div><div class="singleProfileLabel">Bel:</div><div class="singleProfileContent">' + data.nodes[0].node.field_bel + '</div><div class="singleProfileLabel">Kalça:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kalca);
                         }
-                        $("#contentHolder.profiles").append('<div class="singleProfileLabel">Ayak no:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ayak_no + '</div><div class="singleProfileLabel">Göz rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_goz_rengi + '</div><div class="singleProfileLabel">Beceriler:</div><div class="singleProfileContent">' + data.nodes[0].node.field_beceriler + '</div>');
-                        window.scrollTo(0,0);
+                        $("#profile").append('<div class="singleProfileLabel">Ayak no:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ayak_no + '</div><div class="singleProfileLabel">Göz rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_goz_rengi + '</div><div class="singleProfileLabel">Beceriler:</div><div class="singleProfileContent">' + data.nodes[0].node.field_beceriler + '</div>');
                         if (data.nodes[0].node.field_fotograflar.src !== undefined) {
-                            $("#contentHolder.profiles").append('<img class="singleProfileOtherPics" src="' + data.nodes[0].node.field_fotograflar.src + '"/>'); 
+                            $("#profile").append('<img class="singleProfileOtherPics" src="' + data.nodes[0].node.field_fotograflar.src + '"/>'); 
                         } else {
                             $.each(data.nodes[0].node.field_fotograflar, function () {
-                                $("#contentHolder.profiles").append('<img class="singleProfileOtherPics" src="' + this.src + '"/>');
+                                $("#profile").append('<img class="singleProfileOtherPics" src="' + this.src + '"/>');
                             });
                         }
                         if (data.nodes[0].node.field_videolar) {
-                            $("#contentHolder.profiles").append('Videolar');
+                            $("#profile").append('Videolar');
                             $.each(data.nodes, function() {
-                                $("#contentHolder.profiles").append('<a href='+this.node.field_videolar+'><img class="videotn" src='+this.node.field_videolar_thumbnail_path+'/>');
+                                $("#profile").append('<a href='+this.node.field_videolar+'><img class="videotn" src='+this.node.field_videolar_thumbnail_path+'/>');
                             });
                         }
-                        $("#contentHolder.profiles").append(share);
+                        $("#profile").append(share);
                         $("#shareViaEmail").on("click", function() {
                             window.plugins.socialsharing.shareViaEmail('http://www.rejicast.com/node/'+nid);
                         });
