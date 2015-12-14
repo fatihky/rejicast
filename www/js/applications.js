b154cc9ebfdfa89858851c4df6f2f92fa1514797
git <tolga@ozses.net>
2015-12-14 09:51:04 +0200

    Fixed own applications detail not showing

--- www/js/applications.js
+++ www/js/applications.js
@@ -46,23 +46,23 @@ $.ajax({
                                     $("#contentHolder.applications > .profile").remove();
                                     var share = $('<div id="shareViaEmail"><img id="email" src="img/email.png"/></div><div id="shareViaWhatsApp"><img id="whatsapp" src="img/whatsapp.png"/></div>');
                                     var profile = $('<img class="singleProfileImage" src="' + data.nodes[0].node.field_oyuncu_fotografi.src + '"><div class="singleProfileName">' + data.nodes[0].node.field_gosterilecek_ad + '</div><div class="singleProfileLabel">Cinsiyet:</div><div class="singleProfileContent">' + data.nodes[0].node.field_cinsiyet + '</div><div class="singleProfileLabel">Doğum tarihi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_dogum_tarihi + '</div><div class="singleProfileLabel">Şehir:</div><div class="singleProfileContent">' + data.nodes[0].node.field_yasadigi_sehir + '</div><div class="singleProfileLabel">Kategorisi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kategorisi + '</div><div class="singleProfileLabel">Eğitim düzeyi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_egitim_duzeyi + '</div><div class="singleProfileLabel">Diller:</div><div class="singleProfileContent">' + data.nodes[0].node.field_diller + '</div><div class="singleProfileLabel">Kişisel:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kisisel + '</div><div class="singleProfileLabel">Boy:</div><div class="singleProfileContent">' + data.nodes[0].node.field_boy + '</div><div class="singleProfileLabel">Kilo:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kilo + '</div><div class="singleProfileLabel">Ten Rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ten_rengi + '</div>');
-                                    $("#contentHolder.profiles").append(profile);
+                                    $("#contentHolder.applications").append(profile);
                                     if (category) {
-                                        $("#contentHolder.profiles").append('</div><div class="singleProfileLabel">Göğüs:</div><div class="singleProfileContent">' + data.nodes[0].node.field_gogus + '</div><div class="singleProfileLabel">Bel:</div><div class="singleProfileContent">' + data.nodes[0].node.field_bel + '</div><div class="singleProfileLabel">Kalça:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kalca);
+                                        $("#contentHolder.applications").append('</div><div class="singleProfileLabel">Göğüs:</div><div class="singleProfileContent">' + data.nodes[0].node.field_gogus + '</div><div class="singleProfileLabel">Bel:</div><div class="singleProfileContent">' + data.nodes[0].node.field_bel + '</div><div class="singleProfileLabel">Kalça:</div><div class="singleProfileContent">' + data.nodes[0].node.field_kalca);
                                     }
-                                    $("#contentHolder.profiles").append('<div class="singleProfileLabel">Ayak no:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ayak_no + '</div><div class="singleProfileLabel">Göz rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_goz_rengi + '</div><div class="singleProfileLabel">Beceriler:</div><div class="singleProfileContent">' + data.nodes[0].node.field_beceriler + '</div>');
+                                    $("#contentHolder.applications").append('<div class="singleProfileLabel">Ayak no:</div><div class="singleProfileContent">' + data.nodes[0].node.field_ayak_no + '</div><div class="singleProfileLabel">Göz rengi:</div><div class="singleProfileContent">' + data.nodes[0].node.field_goz_rengi + '</div><div class="singleProfileLabel">Beceriler:</div><div class="singleProfileContent">' + data.nodes[0].node.field_beceriler + '</div>');
                                     window.scrollTo(0,0);
                                     if (data.nodes[0].node.field_fotograflar.src !== undefined) {
-                                        $("#contentHolder.profiles").append('<img class="singleProfileOtherPics" src="' + data.nodes[0].node.field_fotograflar.src + '"/>'); 
+                                        $("#contentHolder.applications").append('<img class="singleProfileOtherPics" src="' + data.nodes[0].node.field_fotograflar.src + '"/>'); 
                                     } else {
                                         $.each(data.nodes[0].node.field_fotograflar, function () {
-                                            $("#contentHolder.profiles").append('<img class="singleProfileOtherPics" src="' + this.src + '"/>');
+                                            $("#contentHolder.applications").append('<img class="singleProfileOtherPics" src="' + this.src + '"/>');
                                         });
                                     }
                                     if (data.nodes[0].node.field_videolar) {
-                                        $("#contentHolder.profiles").append('Videolar');
+                                        $("#contentHolder.applications").append('Videolar');
                                         $.each(data.nodes, function() {
-                                            $("#contentHolder.profiles").append('<a href='+this.node.field_videolar+'><img class="videotn" src='+this.node.field_videolar_thumbnail_path+'/>');
+                                            $("#contentHolder.applications").append('<a href='+this.node.field_videolar+'><img class="videotn" src='+this.node.field_videolar_thumbnail_path+'/>');
                                         });
                                     }
                                     $("#contentHolder.applications").append(share);
