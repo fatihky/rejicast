4353426fc16d4f0831f8077c8397ea8c2a629873
Fatih Kaya <1994274@gmail.com>
2016-01-17 12:26:52 +0200

    Integrate OneSignal push notifications.

--- www/js/index.js
+++ www/js/index.js
@@ -3,11 +3,59 @@ document.addEventListener("deviceready", function() {
     checkStatus();
     checkNotifications();
   }
+
+  initPush();
 });
+
+// notification functions - start
+function pushPluginLoaded() {
+  if (!window.plugins || !window.plugins.OneSignal)
+    return false;
+
+  return true;
+}
+
+function initPush() {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  if(!pushPluginLoaded())
+      return console.log('push plugin not loaded.');
+
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal.init("ea091ff0-4b98-4146-afaa-26543c415b67",
+                                {
+                                  googleProjectNumber: "1091017445772",
+                                  autoRegister: true
+                                },
+                                notificationOpenedCallback);
+  
+  // Show an alert box if a notification comes in when the user is in your app.
+  window.plugins.OneSignal.enableInAppAlertNotification(true);
+}
+
+function setPushNotificationUserID(id) {
+  if(!pushPluginLoaded())
+    return;
+
+  window.plugins.OneSignal.sendTag('userID', '' + id);
+}
+
+function unsetPushNotificationUserID() {
+  if(!pushPluginLoaded())
+    return;
+
+  window.plugins.OneSignal.deleteTag('userID');
+}
+
+// notification functions - end
 
 $("#navHolder #homeBtn").on("click", function() {
   window.location.href = "index.html";
 });
+
 $("#navHolder #notificationBtn").on("click", function() {
   window.location.href = "notifications.html";
 });
@@ -35,6 +83,10 @@ function checkStatus() {
             $("a[href='applications.html']").css("display","none");
           } 
           else {
+
+            // set userID
+            setPushNotificationUserID(duser.uid);
+
             $("#loginBtn").css("display","none");
             $("#logoutBtn").css("display","block");
             $("a[href='applications.html']").css("display","block");
@@ -54,6 +106,10 @@ function checkStatus() {
 function logout() {
   navigator.notification.confirm("Çıkış yapmak istiyor musunuz?", function(buttonIndex) {
     if (buttonIndex === 1) {
+
+      // unset userID
+      unsetPushNotificationUserID();
+
       navigator.notification.activityStart("Face in Cast","Çıkış yapılıyor, lütfen bekleyin");
       $.ajax({
         url: 'http://www.rejicast.com/services/user/token.json',
